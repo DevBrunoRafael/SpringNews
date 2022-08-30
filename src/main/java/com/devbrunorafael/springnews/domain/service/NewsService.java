aa750f39ace88dbde2f358fd13bfdaa056023587
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-29 22:32:54 -0300

    RestTemplate add

--- src/main/java/com/devbrunorafael/springnews/domain/service/NewsService.java
+++ src/main/java/com/devbrunorafael/springnews/domain/service/NewsService.java
@@ -1,7 +1,19 @@
 package com.devbrunorafael.springnews.domain.service;
 
+import com.devbrunorafael.springnews.domain.model.News;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class NewsService {
+
+    @Value("${news-api}")
+    private String newsApi;
+
+    public News findLatestNews(String username){
+        return new RestTemplate()
+                .getForEntity(newsApi, News.class).getBody();
+    }
+
 }
