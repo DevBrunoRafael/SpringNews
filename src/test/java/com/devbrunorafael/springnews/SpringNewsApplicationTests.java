34ec14cffd940c97ca2a82d609fd8395a4620155
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-29 22:15:08 -0300

    Initial commit

---
+++ src/test/java/com/devbrunorafael/springnews/SpringNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbrunorafael.springnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
