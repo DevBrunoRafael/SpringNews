b8665268cc31537519a2fd7384c74f10c5d98b48
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-30 21:25:38 -0300

    riding news body

--- src/main/java/com/devbrunorafael/springnews/domain/model/News.java
+++ src/main/java/com/devbrunorafael/springnews/domain/model/News.java
@@ -1,4 +1,15 @@
 package com.devbrunorafael.springnews.domain.model;
 
+import java.time.LocalDate;
+
 public class News {
+
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private LocalDate publishedAt;
+    private String content;
+
 }
