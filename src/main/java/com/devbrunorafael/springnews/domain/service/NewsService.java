d90f499aa4326d2d73c4f3d26860b20630736be3
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-30 21:46:53 -0300

    initiated methods

--- src/main/java/com/devbrunorafael/springnews/domain/service/NewsService.java
+++ src/main/java/com/devbrunorafael/springnews/domain/service/NewsService.java
@@ -5,15 +5,36 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Service
 public class NewsService {
 
     @Value("${news-api}")
     private String newsApi;
 
+    @Value("${news-api-key}")
+    private String newsApiKey;
+
     public News findLatestNews(String username){
         return new RestTemplate()
                 .getForEntity(newsApi, News.class).getBody();
     }
 
+    // aplicar filtro de pais com (country=br&)
+
+    public List<News> findNewsByCountry(String country){
+        String model = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
+        return null;
+    }
+
+    public List<News> findNewsByCategory(String category){
+        String model = "https://newsapi.org/v2/top-headlines?category=business&apiKey=";
+        return null;
+    }
+
+    public List<News> findNewsBySources(String sources){
+        String model = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=";
+        return null;
+    }
 }
