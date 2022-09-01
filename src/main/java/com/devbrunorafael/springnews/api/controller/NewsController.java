0f80d15ebbda29ce09dcffcfcafd8a77f4d65b9c
Bruno Rafael <brunorafaeldls2016@gmail.com>
2022-08-31 21:55:58 -0300

    adding information about endpoints

--- src/main/java/com/devbrunorafael/springnews/api/controller/NewsController.java
+++ src/main/java/com/devbrunorafael/springnews/api/controller/NewsController.java
@@ -2,6 +2,8 @@ package com.devbrunorafael.springnews.api.controller;
 
 import com.devbrunorafael.springnews.domain.model.News;
 import com.devbrunorafael.springnews.domain.service.NewsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,23 +13,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/news")
 @AllArgsConstructor
+@Api(value = "News API")
 public class NewsController {
 
     private NewsService newsService;
 
     @GetMapping("/pais/{country}")
+    @ApiOperation(value = "Search for the main news of the informed country")
     @ResponseStatus(HttpStatus.OK)
     public List<News> findNewsByCountry(@PathVariable String country){
         return newsService.findNewsByCountry(country);
     }
 
     @GetMapping("/categoria/{category}")
+    @ApiOperation(value = "Searches for news of a certain category at an international level")
     @ResponseStatus(HttpStatus.OK)
     public List<News> findNewsByCategory(@PathVariable String category){
         return newsService.findNewsByCategory(category);
     }
 
     @GetMapping("/fonte/{sources}")
+    @ApiOperation(value = "Search for general news in the informed source")
     @ResponseStatus(HttpStatus.OK)
     public List<News> findNewsBySources(@PathVariable String sources){
         return newsService.findNewsBySources(sources);
